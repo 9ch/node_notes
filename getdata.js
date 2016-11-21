13ddc2b2c863125b41eef49d8f9931980a0feac7
9ch <ontheway1024@163.com>
2016-11-21 23:45:28 +0800

    修改为调用配置文件的方式读取url

--- getdata.js
+++ getdata.js
@@ -1,32 +1,26 @@
 var superagent = require("superagent");
 var cheerio = require("cheerio");
+var config = require("config");
 function getItems (){
-	var $data=new Promise(function(resolve,reject){
-
+	var $data = new Promise(function(resolve,reject){
 		superagent
-			.get('http://cnodejs.org/')
-			.end(function(err,res) {
+			.get(config.get("urls"))
+			.end(function (err,res){
 				if(err){
 					reject(err);
 					return ;
 				}
-				var $ =  cheerio.load(res.text);
-				var items=[];
+				var $ = cheerio.load(res.text);
+				var items = [];
 				$(".cell .topic_title_wrapper .topic_title").each(function (index,ele){
-					items.push({
-						'title' : $(ele).attr("title"),
-						'href' : $(ele).attr("href")
-					});
+					items.push([{
+						'title' : $(ele).attr('title'),
+						'href' : $(ele).attr('href')
+					}]);
 				});
 				resolve(items);
 			});
 	});
-
 	return $data;
 }
 module.exports = getItems();
-// module.exports = function (fn){
-// 	getItems(function(items){
-// 		fn(items);
-// 	});
-// }
--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-// var express = require("express");
-// var app = new express();
 var data = require('./getdata.js');
 data.then(doData);
-
 function doData(data){
 	console.log(data);
 }
