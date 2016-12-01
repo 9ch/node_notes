5c99ade67cde815eb24e8163ae70ff4584087fc5
9ch <ontheway1024@163.com>
2016-12-01 11:39:28 +0800

    修复bug

--- getdata.js
+++ getdata.js
@@ -4,61 +4,56 @@ var fs = require('fs');
 function getItems() {
     let $data = new Promise(function (resolve, reject) {//使用promise进行异步操作
         let $text = new Promise(function (resolve, reject) {//先确认是否有这个文件.
-            fs.exists('./config/text.html',function (exist) {
+            fs.exists('./config/text.html', function (exist) {
                 resolve(exist);
             });
         })
-        .then(function (exist) {
-            return new Promise(function (resolve, reject) {
-                if (exist){//获取文件的日期.
-                    fs.stat("./config/text.html",function (err,state) {
-                        if(err) {
-                            console.log(err);
-                        }
-                        resolve(state.atime);
-                    });
-                }
-                else{
-                    getData().then(function (data) {
-                        fs.writeFile("./config/text.html",data,function (err) {
-                            if(err){
-                                console.log(err);
-                                console.log(1);
+            .then(function (exist) {
+                return new Promise(function (resolve, reject) {
+                    if (exist) {//获取文件的日期.
+                        fs.stat("./config/text.html", function (err, state) {
+                            if (err) {
                                 reject(err);
                             }
-                            resolve(new Date());
+                            resolve(state.atime);
                         });
-                    });
-                }
-            })
+                    }
+                    else {
+                        getData().then(function (data) {
+                            fs.writeFile("./config/text.html", data, function (err) {
+                                if (err) {
+                                    reject(err);
+                                }
+                                resolve(new Date());
+                            });
+                        });
+                    }
+                })
 
-        }).then(function (date) {
+            }).then(function (date) {
                 var time = new Date();
-                if((time-date) >= 1000000){
-                    getData().then(function (data) {
-                        fs.writeFile("./config/text.html",data,function (err) {
-                            if(err){
-                                console.log(err);
-                                console.log(2);
+                return new Promise(function (resolve, reject) {
+                    if ((time - date) >= 1000000) {
+                        getData().then(function (data) {
+                            fs.writeFile("./config/text.html", data, function (err) {
+                                if (err) {
+                                    reject(err);
+                                }
+                                resolve(data);
+                            });
+                        })
+                    }
+                    else {
+                        fs.readFile('./config/text.html', function (err, data) {
+                            if (err) {
                                 reject(err);
                             }
-                            resolve(data);
-                        });
-                    })
-                }
-                else{
-                    fs.readFile('./config/text.html',function (err,data) {
-                        if(err){
-                            console.log(err);
-                            console.log(3);
-                            reject(err);
-                        }
-                        else{
-                            resolve(data);
-                        }
-                    })
-                }
-
+                            else {
+                                resolve(data);
+                            }
+                        })
+                    }
+                })
             });
         $text.then(function (data) {//读取相应内容进行筛选处理
             var $ = cheerio.load(data);
@@ -79,13 +74,11 @@ function getData() {
         superagent
             .get('cnodejs.org')
             .end(function (err, data) {
-                if(err){
-                    console.log(err);
-                    console.log(4);
+                if (err) {
                     reject(err);
                 }
-                else{
-                    resolve(data);
+                else {
+                    resolve(data.text);
                 }
             });
     })
